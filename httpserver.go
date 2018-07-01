7b3c31fddc83ef49acb07a8b50b8cedfbe01c31a
tom <thomasrhiggins@gmail.com>
2018-07-01 21:45:23 +1000

    template commit

--- httpserver.go
+++ httpserver.go
@@ -7,18 +7,18 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/schema"
 	"google.golang.org/appengine" // Required external App Engine library
-	mgo "gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
-	//	mgo "gopkg.in/mgo.v2"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/user"
 )
 
 type Person struct {
-	FName string
-	LName string
-	Phone string
-	Email string
+	Fname       string
+	Lname       string
+	Phone       string
+	Email       string
+	NotLoggedIn bool
+	Message     string
 }
 
 type SecurityRoles struct {
@@ -39,12 +39,76 @@ type SecurityRoles struct {
 	L9      string
 }
 
+var UEmail string = "Login"
+var tpl *template.Template
+
+// template_path := filepath.Join(filepath.Dir(root_path), "templates")
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+//var SetupsecurityTmp = parseTemplate("/static/setupSecurity.gtpl")
+
+//listTmpl   = parseTemplate("list.html")
+// 	return listTmpl.Execute(w, r, books)
+func main() {
+
+	var dir string
+
+	flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
+	flag.Parse()
+
+	//r := mux.NewRouter()
+	// r.Handle("/", SayHelloWorld)
+	http.Handle("favicon.ico", http.NotFoundHandler())
+	http.HandleFunc("/getformdata", GetFormDataHandler)
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/setupSecurity", SetupSecurity)
+	http.HandleFunc("/login", Login)
+	http.HandleFunc("/signup", Signup)
+	http.HandleFunc("/postSignup", PostSignupFormDataHandler)
+
+	http.HandleFunc("/postsecuritydata", PostSecurityFormDataHandler)
+	http.HandleFunc("/testtemplate", TestTemplateHandler)
+
+	// This will serve files under http://localhost:8000/static/<filename>
+
+	//r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
+	// http.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	appengine.Main()
+	// r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(Dir))))
+
+	// [START new_key]
+
+}
+func SetUserContext(r *http.Request) Person {
+	ctx := appengine.NewContext(r) //This code setups the username in the header
+	u := user.Current(ctx)
+	UEmail = fmt.Sprintf("%v", u) // it is here because Context changes before templates are excuted
+	p1 := new(Person)
+	p1.Email = UEmail
+
+	if u == nil {
+
+		p1.Email = "Login"
+		p1.Message = "Sign Up"
+		p1.NotLoggedIn = true
+
+		//	UEmail = "Login"
+	} else {
+		p1.Message = "Welcome"
+	}
+	return *p1
+}
+
 func SetupSecurity(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //get request method
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("static/setupSecurity.gtpl")
+		// tpl.ExecuteTemplate(w, "setupSecurity.gohtml", SetUserContext(r))
+		t, _ := template.ParseFiles("ui/setupSecurity.gohtml")
 		t.Execute(w, nil)
-		//	w.Write([]byte(fmt.Sprintf(html)))
+		// not included in olde script 	w.Write([]byte(fmt.Sprintf(html)))
 	} else {
 		r.ParseForm()
 	}
@@ -52,12 +116,28 @@ func SetupSecurity(w http.ResponseWriter, r *http.Request) {
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r) //This code setups the username in the header
+	u := user.Current(ctx)
+	UEmail = fmt.Sprintf("%v", u) // it is here because Context changes before templates are excuted
+	p1 := new(Person)
+	p1.Email = UEmail
 
-	fmt.Println("method:", r.Method) //get request method
+	if u == nil {
+
+		p1.Email = "Login"
+		p1.Message = "Sign Up"
+		p1.NotLoggedIn = true
+
+		//	UEmail = "Login"
+	} else {
+		p1.Message = "Welcome"
+	}
+
+	//get request method
 	if r.Method == "GET" {
 
 		t, _ := template.ParseFiles("static/index.html")
-		t.Execute(w, nil) //fmt.Printf(r.URL.Path[1:])
+		t.Execute(w, p1) //fmt.Printf(r.URL.Path[1:])
 	} else {
 	}
 
@@ -86,107 +166,101 @@ func GetFormDataHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte(fmt.Sprintf(html)))
 }
-
 func Login(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	ctx := appengine.NewContext(r)
+	u := user.Current(ctx)
+	// UEmail = fmt.Sprintf("%v", u)
+
+	log.Printf("current user: %v ", UEmail)
+	log.Printf("%#v", u)
+	log.Printf("%#v", ctx)
+
+	if u == nil {
+
+		url, _ := user.LoginURL(ctx, "/")
+		fmt.Fprintf(w, `<a href="%s">Sign in or register</a>`, url)
+
+	}
+
+	url, _ := user.LogoutURL(ctx, "/")
+	fmt.Fprintf(w, `<a Welcome, %s\n ! (<a href="%s">sign out</a>`, u, url)
+
+}
+
+func Signup(w http.ResponseWriter, r *http.Request) {
 	// fmt.Println("method:", r.Method) //get request method
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("static/login.gtpl")
-		t.Execute(w, nil)
+		tpl.ExecuteTemplate(w, "signup.gohtml", SetUserContext(r))
+		//	tpl, _ := template.ParseFiles("/templates/signup.old.gohtml")
+		//	tpl.Execute(w, SetUserContext(r))
 	} else {
 		r.ParseForm()
 		// logic part of log in
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
 
 	}
+
 }
 
-func PostFormDataHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+func TestTemplateHandler(w http.ResponseWriter, r *http.Request) {
 
-	if err != nil {
-		fmt.Println(" 149 error is ", err)
-	}
-
-	//person := new(Person)
-	org := new(SecurityRoles)
-	decoder := schema.NewDecoder()
-
-	err = decoder.Decode(org, r.PostForm)
-
-	if err != nil {
-		fmt.Println("159 err0r ", err)
-		//	mongo()
-	}
-	fmt.Println(" 121 before mongo call")
-	mongo(*org)
-	// fmt.Println(person)
-	fmt.Println(org)
-	fmt.Println(org.State)
-
-	w.Write([]byte(fmt.Sprintf("org  is:  %v \n", org)))
-	//	w.Write([]byte(fmt.Sprintf("\n L9 is : %v \n", org.l2))) */
+	tpl.ExecuteTemplate(w, "index.gohtml", SetUserContext(r))
 }
+func PostSignupFormDataHandler(w http.ResponseWriter, r *http.Request) {
 
-func main() {
+	r.ParseForm()
 
-	var dir string
-
-	flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
-	flag.Parse()
-	//r := mux.NewRouter()
-	// r.Handle("/", SayHelloWorld)
-	http.HandleFunc("/getformdata", GetFormDataHandler)
-	http.HandleFunc("/", IndexHandler)
-	http.HandleFunc("/setupSecurity", SetupSecurity)
-	http.HandleFunc("/login", Login)
-	http.HandleFunc("/postsecuritydata", PostFormDataHandler)
-	// This will serve files under http://localhost:8000/static/<filename>
-
-	//r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
-	// http.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
-	appengine.Main()
-	// r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(Dir))))
-
-	//	r.PathPrefix("static").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
-	/* Replace everything below with apppengnine and chane calls above to from r. to http to suit the appengine model.
-		srv := &http.Server{
-			Handler: r,
-			Addr:    "localhost:8080",
-			// Good practice: enforce timeouts for servers you create!
-			WriteTimeout: 15 * time.Second,
-			ReadTimeout:  15 * time.Second,
-		}
-
-		log.Fatal(srv.ListenAndServe())
-	}*/
+	p1 := Person{
+		Fname: r.Form.Get("fname"),
+		Lname: r.Form.Get("lname"),
+		Email: r.Form.Get("email"),
+	}
+	log.Printf("person is ", p1.Fname)
+	//uname := r.Form.Get["username"]
+	//fmt.Fprintf(w, " %s\n  username:", r.Form["username"])
+	//fmt.Fprintf(w, "%s\n password:", r.Form["password"])
+	//fmt.Fprintf(w, "%s\n ")
+	//func Current(c context.Context) *User
 }
-func mongo(o SecurityRoles) {
-	fmt.Println("158 mongo called")
-	session, err := mgo.Dial("localhost")
-	if err != nil {
-		panic(err)
+func PostSecurityFormDataHandler(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+	// [START new_context]
+	ctx := appengine.NewContext(r)
+	// [END new_context]
+	s1 := SecurityRoles{
+
+		Admin:   r.Form.Get("admin"),
+		Orgname: r.Form.Get("orgname"),
+		Country: r.Form.Get("country"),
+		State:   r.Form.Get("state"),
+		City:    r.Form.Get("city"),
+		Sname:   r.Form.Get("sname"),
+		L1:      r.Form.Get("l1"),
+		L2:      r.Form.Get("l2"),
+		L3:      r.Form.Get("l3"),
+		L4:      r.Form.Get("l4"),
+		L5:      r.Form.Get("l5"),
+		L6:      r.Form.Get("l6"),
+		L7:      r.Form.Get("l7"),
+		L8:      r.Form.Get("l8"),
+		L9:      r.Form.Get("l9"),
 	}
-	defer session.Close()
-
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-
-	c := session.DB("security").C("organisation")
-	fmt.Println("168 before insert statement")
-	err = c.Insert(&SecurityRoles{"Admin", o.Orgname, o.Country, o.State, o.City, o.Sname, o.L1, o.L2, o.L3, o.L4, o.L5, o.L6, o.L7, o.L8, o.L9})
-
+	key := datastore.NewIncompleteKey(ctx, "securityroles", nil)
+	// [END new_key]
+	// [START add_post]
+	key, err := datastore.Put(ctx, datastore.NewIncompleteKey(ctx, "securityroles", nil), &s1)
 	if err != nil {
-		fmt.Println("173 Mongo Error ")
-		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	result := SecurityRoles{}
-	err = c.Find(bson.M{"city": "Bathurst"}).One(&result)
-	if err != nil {
-		log.Fatal(err)
+	var s2 SecurityRoles
+	if err = datastore.Get(ctx, key, &s2); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	fmt.Println("School Name: ", result.Sname)
+	fmt.Fprintf(w, "Stored and retrieved the School named %q", s2.Sname)
 }
